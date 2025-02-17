f9b7c7674c2bfbbc003ecf1d80dbaeecd55a7c36
T.MiuraPro <mtaiga0213@icloud.com>
2025-02-17 18:49:03 +0900

    feat: buttonコンポーネントの作成

--- react-todo/todo-project/src/components/Button.jsx
+++ react-todo/todo-project/src/components/Button.jsx
@@ -1,2 +1,19 @@
 //編集ボタン
 //完了ボタン
+import { useTheme } from "../contexts/ThemeContext";
+import { useDispatch } from "../contexts/TodoContext";
+const Button = ({ text, type, todo }) => {
+  const { theme } = useTheme();
+  const { dispatch } = useDispatch();
+  
+  const buttonhandler = () => {
+    dispatch({ type, todo });
+  };
+  return (
+    <button className={`button $(theme)`} onClick={buttonhandler}>
+      {text}:{type}
+    </button>
+  );
+};
+
+export default Button;
--- react-todo/todo-project/src/components/Todo/Todo.jsx
+++ react-todo/todo-project/src/components/Todo/Todo.jsx
@@ -5,14 +5,14 @@
 // <List todo={todo}>
 //)
 //})
-import { useConst, useDispatch } from "../../contexts/TodoContext";
+import { useDispatch } from "../../contexts/TodoContext";
 import { useTheme } from "../../contexts/ThemeContext";
+import { useConst } from "../../contexts/TodoContext";
 import Button from "../Button";
 export const Todo = () => {
-  const { ADD, DELETE, DONE, UPDATE } = useConst();
   const { theme } = useTheme();
   const { state } = useDispatch();
-
+  const { UPDATE, DELETE, DONE } = useConst();
   return (
     <div>
       {state.map((prev) => {
@@ -23,9 +23,9 @@ export const Todo = () => {
               <li className={`Todo-Card_item ${theme}`}>{prev.createdate}</li>
               <li className={`Todo-Card_item ${theme}`}>{prev.due}</li>
               <li className={`Todo-Card_item ${theme}`}>{prev.priority}</li>
-              <Button text={"編集"} />
-              <Button text={"完了"} />
-              <Button text={"削除"} />
+              <Button text={"編集"} type={UPDATE} todo={prev} />
+              <Button text={"完了"} type={DONE} todo={prev} />
+              <Button text={"削除"} type={DELETE} todo={prev} />
             </ul>
           </section>
         );
